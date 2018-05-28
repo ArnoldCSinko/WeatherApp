330d13e3a4b77a39bbd6b5ccf9ac43fe3b9d31dc
Chris Sinko <ArnoldCSinko@gmail.com>
2018-05-28 18:22:36 -0400

    Refactor with ES6 syntax

--- js/app.js
+++ js/app.js
@@ -1,72 +1,116 @@
 /*
     Written By: Arnold C. Sinko
-    Updated: 1/29/18
+    Updated: 5/27/2018
 */
-const appID = /* Omitted */;
-const apiURL = "http://api.openweathermap.org/data/2.5/weather?q=";
 
-function Weather(country, city, tempK, tempF, tempC, condition, icon) {
-    this.country = country;
-    this.city = city;
-    this.tempK = tempK;
-    this.tempF = tempF;
-    this.tempC = tempC;
-    this.condition = condition;
-    this.icon = icon;
+
+/*
+    Weather Class: Defines a weather object.
+    
+*/
+class Weather {
+    constructor(country, city, tempK, tempC, tempF, condition, icon) {
+        this._country = country;
+        this._city = city;
+        this._tempK = tempK;
+        this._tempF = tempF;
+        this._tempC = tempC;
+        this._condition = condition;
+        this._icon = icon;
+    }
+
+    get country() {
+        return this._country;
+    }
+
+    get city() {
+        return this._city;
+    }
+
+    get tempK() {
+        return this._tempK;
+    }
+
+    get tempC() {
+        return this._tempC;
+    }
+    get tempF() {
+        return this._tempF;
+    }
+
+    get condition() {
+        return this._condition;
+    }
+
+    get icon() {
+        return this._icon;
+    }
+
 }
 
-// function displays weather information to user from given weather object
-function displayWeather(weather) {
+/*
+    Helper functions
+*/
 
-    $('#location').html(weather.country + ', ' + weather.city);
-    $('#tempF').html(weather.tempF + '<span class="tempDeg"> ' + String.fromCharCode(176) + 'F</span>');
-    $('#tempC').html(weather.tempC + '<span class="tempDeg"> ' + String.fromCharCode(176) + 'C</span>');
-    $('#condition').html(weather.condition.charAt(0).toUpperCase() + weather.condition.slice(1) + '<span class="imageSpan"><img class="weatherImg" alt="Weather Iamge" /></span>');
-    $('.weatherImg').attr('src', weather.icon);
+// Displays weather information to user from given weather object
+function displayWeather({ country, city, tempC, tempF, condition, icon }) {
+    $('#location').html(country + ', ' + city);
+    $('#tempF').html(tempF + '<span class="tempDeg"> ' + String.fromCharCode(176) + 'F</span>');
+    $('#tempC').html(tempC + '<span class="tempDeg"> ' + String.fromCharCode(176) + 'C</span>');
+    $('#condition').html(condition.charAt(0).toUpperCase() + condition.slice(1) + '<span class="imageSpan"><img class="weatherImg" alt="Weather Iamge" /></span>');
+    $('.weatherImg').attr('src', icon);
 }
 
+// Create a Weather object from JSON data
+function makeWeatherObject(data) {
+    let iconURL = 'http://api.openweathermap.org/img/w/';
+    const { description: condition, icon } = data.weather[0];
+    iconURL += icon;
+    const city = data.name;
+    const country = data.sys.country;
+    const tempK = parseInt(data.main.temp);
+    const { tempF, tempC } = convertFromKelvin(tempK);
+    return new Weather(country, city, tempK, tempC, tempF, condition, iconURL);
+}
+
+// Make AJAX request for JSON weather data
+// Then pass the weather object to the function displayWeather(WeatherObject).
+function getWeather(location) {
+    const appID = "&appid=82779f2d14e57a0b79d137998db504ca";
+    let apiURL = "http://api.openweathermap.org/data/2.5/weather?q=";
+    apiURL += location + appID;
+    $.getJSON(apiURL, function (data) {
+        const localWeather = makeWeatherObject(data);
+        displayWeather(localWeather);
+    });
+}
+
+// Return object containing temperature data converted from Kelvin
+function convertFromKelvin(tempK) {
+    return {
+        tempF: Math.round(tempK * (9 / 5) - 459.67),
+        tempC: Math.round(tempK - 273.15)
+    }
+}
+
+/*  
+    Main
+*/
 $(function () {
     //Make AJAX request for JSON data of user location
     var locationAPIURL = "https://ipinfo.io/json";
-    $.getJSON(locationAPIURL, function(location) {
-        var city = location.city;
-        var country = location.country;
-        var latLonArray = location.loc.split(',');
-        var locString = city + ',' + country;
+    $.getJSON(locationAPIURL, function (location) {
+        const { city, country } = location;
+        const locString = city + ',' + country;
         //append apiURL String with users location and appID.
-        apiURL += locString + appID;
-        getWeather();
-
+        getWeather(locString);
     });
+
     // toggle between fahrenheit and celsius
     $('.temp').on('click', function () {
         $('.temp').toggleClass('hidden');
     });
 
-
 });
 
 
-//Make AJAX request for JSON weather data and create a new weather object with the data.
-//Then pass the weather object to the function displayWeather(WeatherObject).
-function getWeather() {
-    var iconURL = 'http://api.openweathermap.org/img/w/';
-    $.getJSON(apiURL, function (data) {
-
-        var localWeather = new Weather();
-        localWeather.condition = data.weather[0].description;
-        iconURL += data.weather[0].icon;
-        localWeather.icon = iconURL;
-        localWeather.city = data.name;
-        localWeather.country = data.sys.country;
-        var tempK = parseInt(data.main.temp);
-        localWeather.tempK = tempK;
-        var tempF = Math.round(tempK * (9 / 5) - 459.67);
-        var tempC = Math.round(tempK - 273.15);
-        localWeather.tempF = tempF;
-        localWeather.tempC = tempC;
-        displayWeather(localWeather);
-
-    });
-
-}
